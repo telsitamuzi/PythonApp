f5355d48bca09d4fafc6b075a2a2d04d42ea6a1f
Nicholas Jackson <njacks30@uncc.edu>
2021-04-17 15:07:27 -0400

    edited the datebase file to include event db

--- models.py
+++ models.py
@@ -10,3 +10,16 @@ class user(db.Model):
         self.password = password
         self.fName - fName
         self.lName = lName
+
+class event(db.Model):
+    creator_id = db.column("creator_id", db.Integer, primary_key=True)
+    event_name = db.coumn("event_name", db.String(100))
+    start_date = db.column("start_date", db.String(100))
+    end_date = db.column("end_date", db.String(100))
+    event_details = db.column("event_details", db.String(100))
+
+    def __init__(self, event_name, start_date, end_date, event_details):
+        self.event_name = event_name
+        self.start_date = start_date
+        self.end_date = end_date
+        self.event_details = event_details
