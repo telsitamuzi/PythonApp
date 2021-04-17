383d601e078213dcd7a0fcf9a730a4e01fb5e806
Nathan Hartzell <nhartzel@uncc.edu>
2021-04-17 14:46:09 -0400

    updated application file for database

--- application.py
+++ application.py
@@ -4,11 +4,19 @@ import os  # os is used to get environment variables IP & PORT
 from flask import Flask, redirect, url_for  # Flask is the web app that we will customize
 from flask import render_template
 from flask import request
+from database import db
+
 
 app = Flask(__name__)  # create an app
 
-with app.app_context():
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_project_app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
 
+db.init_app(app) # initializes database
+
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/index')
 def index():
